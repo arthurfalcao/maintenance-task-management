fc6b2b9a6ab32d238388b74a8d27db1aada417c0
Arthur Falcão <arthurjvfalcao@gmail.com>
2022-03-31 21:01:04 +0100

    refactor: :fire: remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AuthModule, TasksModule],
 })
 export class AppModule {}
