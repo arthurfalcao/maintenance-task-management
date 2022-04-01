76aee67e70015c927c33b1d5ccb7ce850bbf43f7
Arthur Falcão <arthurjvfalcao@gmail.com>
2022-04-01 22:55:24 +0100

    test: :white_check_mark: add support to e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
 import { NotificationModule } from './notification/notification.module';
 
+const environment = process.env.NODE_ENV || 'development';
+
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${environment}`, `.env.${environment}.local`],
+      isGlobal: true,
+      expandVariables: true,
+    }),
     PrismaModule,
     AuthModule,
     TasksModule,
